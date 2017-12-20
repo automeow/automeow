26abd263e239e3a0455e2f87509855d00aa6fde3
Sam Boylett <sam.boylett@piksel.com>
2017-12-20 12:42:52 +0000

    Falling Meow

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <meow />
+  </div>
+</template>
+
+<script>
+import Meow from './components/meow'
+
+export default {
+  name: 'app',
+  components: {
+    Meow
+  }
+}
+</script>
+
+<style lang="less">
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  overflow: hidden;
+
+  #app {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+  }
+}
+</style>
---
+++ src/components/meow.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="meow"
+    ref="element"
+    v-bind:style="{
+      top: `${top}%`,
+      left: `${left}%`
+    }">
+    Meow
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'meow',
+  data () {
+    return {
+      top: -10,
+      left: Math.floor(Math.random() * 100)
+    }
+  },
+  mounted () {
+    setTimeout(this.beginMovement.bind(this), 0)
+  },
+  methods: {
+    beginMovement () {
+      this.top = 100
+
+      setTimeout(this.endMovement.bind(this), 16000)
+    },
+    endMovement () {
+      this.$emit('ended')
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.meow {
+  position: absolute;
+  transition: top 15s linear;
+  animation: drift 5s ease-in-out infinite;
+}
+
+@keyframes drift {
+  0% {
+    transform: translateX(-300%);
+  }
+  50% {
+    transform: translateX(300%);
+  }
+  100% {
+    transform: translateX(-300%);
+  }
+}
+</style>
